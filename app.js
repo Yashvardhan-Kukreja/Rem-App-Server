e82fd220acb75258d82c514ee264808a2390d2c0
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-03-18 01:22:47 +0530

    Added routes and some dependencies to the main script

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
+const logger = require('morgan');
+
 const config = require('./config');
+const patientRoutes = require('./routes/patientRoutes');
+const authenticationRoutes = require('./routes/authenticateRoutes');
+
 const db = config.database;
 const port = process.env.PORT || 8000;
 const app = express();
 
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/authenticate', authenticationRoutes);
+app.use('/patient', patientRoutes);
+
 
 mongoose.connect(db, function(err){
     if (err){
